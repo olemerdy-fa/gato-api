b5a244a1cbee93a6d9130e35083e9bdba0c8c589
Olivier Le Merdy <olemerdy@financeactive.com>
2018-04-26 21:34:10 +0200

    Auth0 security config

---
+++ src/main/java/com/financeactive/gatoapi/rest/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.financeactive.gatoapi.rest.security;
+
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${auth0.apiAudience}")
+    private String apiAudience;
+
+    @Value("${auth0.issuer}")
+    private String issuer;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        JwtWebSecurityConfigurer
+                .forRS256(apiAudience, issuer)
+                .configure(http)
+                .authorizeRequests()
+                .anyRequest()
+                .permitAll();
+    }
+}
