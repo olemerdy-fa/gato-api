710e3d18a942dd94b3bc51df7f80a0de5c17a30a
Olivier Le Merdy <olemerdy@financeactive.com>
2018-05-02 09:14:36 +0200

    Random creation date

--- src/main/java/com/financeactive/gatoapi/core/stuff/StuffGen.java
+++ src/main/java/com/financeactive/gatoapi/core/stuff/StuffGen.java
@@ -3,9 +3,10 @@ package com.financeactive.gatoapi.core.stuff;
 import com.github.javafaker.Faker;
 import org.springframework.stereotype.Component;
 
+import java.sql.Date;
 import java.time.Clock;
-import java.time.Instant;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
 @Component
@@ -24,7 +25,7 @@ public class StuffGen implements Supplier<Stuff> {
     public Stuff get() {
         return new Stuff(
                 UUID.randomUUID(),
-                Instant.now(clock),
+                faker.date().past(1_000, TimeUnit.DAYS, Date.from(clock.instant())).toInstant(),
                 faker.book().title(),
                 faker.lorem().paragraph()
         );
