9c7d97474d51da97225028fea126354ab107fba7
Olivier Le Merdy <olemerdy@financeactive.com>
2018-06-07 14:04:46 +0200

    CORS on user endpoint

--- src/main/java/com/financeactive/gatoapi/rest/security/SecurityController.java
+++ src/main/java/com/financeactive/gatoapi/rest/security/SecurityController.java
@@ -1,16 +1,20 @@
 package com.financeactive.gatoapi.rest.security;
 
+import com.google.common.collect.ImmutableMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.Map;
 
+@CrossOrigin
 @RestController
 public class SecurityController {
 
     @GetMapping("user")
-    public Principal user(Principal principal) {
-        return principal;
+    public Map<String, Object> user(Principal principal) {
+        return ImmutableMap.of("name", principal.getName());
     }
 
 }
